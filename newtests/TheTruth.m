594407e764f4ee42d13840ed122a38d50bc7e61c
Umakant Shekokar <ushekoka@mathworks.com>
2021-02-21 13:46:49 +0530

    Add some tests

---
+++ newtests/TheTruth.m
@@ -0,0 +1,15 @@
+classdef TheTruth < matlab.unittest.TestCase
+    methods (Test)
+        function testTheTestOfAllTime(testCase)
+            onetyone = 11;
+            testCase.verifyEqual(onetyone, 11);
+        end
+    end
+
+    methods (Test, TestTags = {'Unit'})
+        function testTheTag(testCase)
+            val = 10;
+            testCase.verifyEqual(val,10);
+        end
+    end
+end
